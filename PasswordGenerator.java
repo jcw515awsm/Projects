487a4adaa702e1d0fcb9274a7ef970e7d5eaa115
Julius White <awsmguytoo@gmail.com>
2022-01-05 17:47:36 -0600

    current password generator; currently adding a GUI

--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -1,7 +1,7 @@
-import java.lang.*;
 import java.util.Scanner;
 
 class PasswordGenerator {
+
         public int digit;
         private String lower_case = "qwertyuiopasdfghjklzxcvbnm";
         private String upper_case = "QWERTYUIOPASDFGHJKLZXCVBNM";
@@ -12,16 +12,20 @@ class PasswordGenerator {
                 String value = lower_case + upper_case + symbols;
                 double random = value.length()*Math.random();
                 int randomcharacter = (int)random;
-                
                 System.out.print(value.charAt(randomcharacter));
 
             }
+        void ask() {
+            JFrame frame = new JFrame();
+            J
+
+        }
         }
     public static void main (String[] args) {
         PasswordGenerator pg = new PasswordGenerator();
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Enter length of password: ");
+        System.out.print("Enter length of password: ");
         pg.digit = scan.nextInt();
         System.out.print("Your new password is: ");
         pg.go();
