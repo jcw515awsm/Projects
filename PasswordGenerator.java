9b54b8f4642fc17a2957f0e3f1e009a4dcfaca9e
Julius White <awsmguytoo@gmail.com>
2022-01-05 18:38:59 -0600

    adding GUI

--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.swing.*;
 
 class PasswordGenerator {
 
@@ -15,21 +16,23 @@ class PasswordGenerator {
                 System.out.print(value.charAt(randomcharacter));
 
             }
-        void ask() {
-            JFrame frame = new JFrame();
-            J
-
         }
-        }
+    }
     public static void main (String[] args) {
+
+        JFrame frame = new JFrame();
+        JTextBox textbox = new JTextBox("Enter Length of Password");
+        frame.getContentPane().add(textbox);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         PasswordGenerator pg = new PasswordGenerator();
         Scanner scan = new Scanner(System.in);
-
         System.out.print("Enter length of password: ");
         pg.digit = scan.nextInt();
         System.out.print("Your new password is: ");
         pg.go();
     }
-
 }
 
