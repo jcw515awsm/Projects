4f0d7441fdbafe2b7772b4949b6a387bc2057f1d
Julius White <awsmguytoo@gmail.com>
2022-01-29 22:15:33 -0600

    added a few specs in algorithm

--- sortalgo.java
+++ sortalgo.java
@@ -1,3 +1,9 @@
 public class sort{
-    
+    public static int[] numbers = {7,9,3,4,0};
+
+    public static void main(String[] args) {
+        for (int i = 0; i < numbers.length; i++){
+            
+        }
+    }
 }
